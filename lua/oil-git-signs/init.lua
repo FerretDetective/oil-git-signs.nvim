8d3e6d49816472162b44940e4be9a7ff2ea220a4
Eris Fletcher <theferretdetective@gmail.com>
2024-09-16 00:23:53 -0600

    fix(formatting): remove trailing space

--- lua/oil-git-signs/init.lua
+++ lua/oil-git-signs/init.lua
@@ -379,7 +379,7 @@ M.defaults = {
         [M.GitStatus.IGNORED]          = nil,
         -- stylua: ignore end
     },
-    -- used to create buffer local keymaps when oil-git-signs attaches to a buffer 
+    -- used to create buffer local keymaps when oil-git-signs attaches to a buffer
     -- note: the buffer option will always be overwritten
     ---@type { [1]: string|string[], [2]: string, [3]: string|function, [4]: vim.keymap.set.Opts? }[]
     keymaps = {},
