6386d39744127be8d2a800e3d255c7af8006a469
Eris Fletcher <theferretdetective@gmail.com>
2025-04-02 01:43:01 -0600

    refactor: use `@private` tag for private methods and fields

--- lua/oil-git-signs/watcher.lua
+++ lua/oil-git-signs/watcher.lua
@@ -1,8 +1,8 @@
 ---@class oil_git_signs.FsWatcher
----@field _filepath string
----@field _callbacks table<integer, nil|fun(err: string|nil, filename: string, events: uv.fs_event_start.callback.events)>
----@field _current_handle integer
----@field _fs_event uv.uv_fs_event_t|nil
+---@field private filepath string
+---@field private callbacks table<integer, nil|fun(err: string|nil, filename: string, events: uv.fs_event_start.callback.events)>
+---@field private current_handle integer
+---@field private fs_event uv.uv_fs_event_t|nil
 local FsWatcher = {}
 
 ---Create a new `FsWatcher`
@@ -11,10 +11,10 @@ local FsWatcher = {}
 function FsWatcher.new(filepath)
     local self = setmetatable({}, { __index = FsWatcher })
 
-    self._filepath = filepath
-    self._callbacks = {}
-    self._current_handle = 0
-    self._fs_event = nil
+    self.filepath = filepath
+    self.callbacks = {}
+    self.current_handle = 0
+    self.fs_event = nil
 
     return self
 end
@@ -23,8 +23,8 @@ end
 ---@param callback fun(err: string|nil, filename: string, events: uv.fs_event_start.callback.events)
 ---@return integer handle
 function FsWatcher:register_callback(callback)
-    local handle = self:_get_new_handle()
-    self._callbacks[handle] = callback
+    local handle = self:get_new_handle()
+    self.callbacks[handle] = callback
 
     return handle
 end
@@ -36,8 +36,8 @@ end
 ---@overload fun(handle: integer): true, fun(err: string|nil, filename: string, events: uv.fs_event_start.callback.events)
 ---@overload fun(handle: integer): false, nil
 function FsWatcher:deregister_callback(handle)
-    local callback = self._callbacks[handle]
-    self._callbacks[handle] = nil
+    local callback = self.callbacks[handle]
+    self.callbacks[handle] = nil
 
     return callback ~= nil, callback
 end
@@ -54,10 +54,10 @@ function FsWatcher:start(flags)
     if err ~= nil then
         return false, err, err_name
     end
-    self._fs_event = assert(fs_event)
+    self.fs_event = assert(fs_event)
 
-    _, err, err_name = self._fs_event:start(self._filepath, flags or {}, function(...)
-        for _, cb in ipairs(self._callbacks) do
+    _, err, err_name = self.fs_event:start(self.filepath, flags or {}, function(...)
+        for _, cb in ipairs(self.callbacks) do
             pcall(cb, ...)
         end
         self:stop()
@@ -73,18 +73,19 @@ end
 ---@overload fun(): true, nil, nil
 ---@overload fun(): false, uv.error.message?, uv.error.name?
 function FsWatcher:stop()
-    if self._fs_event == nil then
+    if self.fs_event == nil then
         return true, nil, nil
     end
 
-    local _, err, err_name = vim.uv.fs_event_stop(self._fs_event)
+    local _, err, err_name = vim.uv.fs_event_stop(self.fs_event)
 
     return err == nil, err, err_name
 end
 
-function FsWatcher:_get_new_handle()
-    self._current_handle = self._current_handle + 1
-    return self._current_handle
+---@private
+function FsWatcher:get_new_handle()
+    self.current_handle = self.current_handle + 1
+    return self.current_handle
 end
 
 return FsWatcher
